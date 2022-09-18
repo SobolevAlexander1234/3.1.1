33575f48e79bfc858bdab8880a67daadf691c668
SobolevAlexander1234 <108826531+SobolevAlexander1234@users.noreply.github.com>
2022-09-18 12:54:24 +0300

    Add files via upload

---
+++ Spring_boot_3.1.1/src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,23 @@
+package com.example.demo.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "age")
+    private int age;
+}
